2a2e51f9ee60dce82511d21c3ffebea347d7867e
Stefan Solmundson <stefan.solmundson@gmail.com>
2019-11-21 18:53:35 +0800

    EditAdd.js now displays services data records correctly when they are selected, however EditAdd.js still doesn't work for the other data sets...

--- src/components/EditAdd.js
+++ src/components/EditAdd.js
@@ -5,9 +5,9 @@ import React, { Component } from 'react';
 import { MDBBtn, MDBContainer, MDBRow } from "mdbreact";
 
 export class EditAdd extends Component {
-  
-  constructor(props) {
-    super(props);
+
+  constructor( props ) {
+    super( props );
 
     let _record = undefined;
     if ( this.props.match.params.operation === "add" ) {
@@ -68,20 +68,17 @@ export class EditAdd extends Component {
       // duplicate VIN
       else if ( this.props.data.find( element => element.VIN === this.state.record.VIN ) ) {
         alert( "Sorry, your record must have a unique VIN." );
-      }
-      else {
+      } else {
         // call addRecord on App.js, then navigate to "/vehicles/" (ShowData) AFTER the setState in App.js has finished by using the setStates callback
-        this.props.addRecord( this.state.record, this.props.dataArrayName, () => this.props.history.push( `/${this.props.dataArrayName}/` ) );
+        this.props.addRecord( this.state.record, this.props.dataArrayName, () => this.props.history.push( `/${ this.props.dataArrayName }/` ) );
       }
     } else {
       // call editRecord on App.js, then navigate to "/vehicles/" (ShowData) AFTER the setState in App.js has finished by using the setStates callback
-      this.props.editRecord( this.state.record, this.props.dataArrayName, () => this.props.history.push( `/${this.props.dataArrayName}/` ) );
+      this.props.editRecord( this.state.record, this.props.dataArrayName, () => this.props.history.push( `/${ this.props.dataArrayName }/` ) );
     }
   };
 
-  onReset = ( e ) => {
-    e.preventDefault();
-  };
+  // TODO: how to make the inputs look good https://reactstrap.github.io/components/form/
 
   render() {
     // Conditions to check it is not "just" the vehicles page
@@ -89,7 +86,56 @@ export class EditAdd extends Component {
     // OK SO MOST OF THESE dataDefaultRecord's ARE MEANT TO BE records because this is both an editting & adding form
     const { record } = this.state;
     const { columnHeadings } = this.props;
-    console.log("this.props", this.props );
+    console.log( "this.props", this.props );
+    console.log( "record", record );
+    console.log( "Object.keys(record).length", Object.keys( record ).length );
+    const offset = Object.keys( record ).length - 2;
+    console.log( "record.vehicleServicesArrray", record.vehicleServicesArray );
+
+    console.log( "record.vehicleServicesArray.map( sField => console.log( sField ) );\n\n*below" );
+    record.vehicleServicesArray.map( sField => console.log( sField ) );
+
+    // record.vehicleServicesArray == field
+    let field = record.vehicleServicesArray;
+    console.log( "field is an array, here is field:", field );
+    record.vehicleServicesArray.map( sField => console.log( "name's of the objects within the sField Array\n-->\nssField names: ", Object.keys( sField ) ) );
+    // THEN
+    field.map( sField =>
+      console.log( "field contains elements,\n" +
+        "each of field's elements is called an sField because they are inside of field,\n" +
+        "each sField is an object,\n" +
+        "here are the sFields:", sField )
+    );
+
+    console.log( "here are the arrays of keys from each sField: ",
+      field.map( sField =>
+        console.log( Object.keys( sField ) )
+      )
+    );
+    console.log( "field are arrays,\nsfield's are objects,\nssfield's are properties;\n" );
+
+    field.map( sField => {
+      console.log( "new sfield" );
+      Object.keys( sField ).map( ssFieldKey =>
+        console.log( "here are the key, value pair of the sfields within field", ssFieldKey, sField[ ssFieldKey ] ) )
+    } );
+
+    // I need to map through the keys in each sField, I need to use these keys to access the properties within each sField
+
+    record.vehicleServicesArray.map(
+      sField => Object.keys( sField )
+    ).map( ssFieldName =>
+      record.vehicleServicesArray.map(
+        sFieldArray => console.log( ssFieldName, sFieldArray[ ssFieldName ] )
+      )
+    );
+    // So I need to access the subfield arrays & used the ssField names on them to access their properties
+    // record.vehicleServicesArray.map(
+    //   sField => Object.keys( sField )
+    // ).map(
+    //   ssFieldName => ss
+    // );
+    // const thing = this.props.match.params.operation === "add" ? "field === 'vid'" : undefined;
 
     return (
       <MDBContainer className="p-3">
@@ -102,38 +148,64 @@ export class EditAdd extends Component {
               (
                 Object.keys( record ).map( ( field, index ) => (
                   <>
-                    <MDBRow>
-                      <label htmlFor={ field } className="grey-text">
-                        { columnHeadings[ index ].label }
-                      </label>
-                    </MDBRow>
-                    <MDBRow>
-                      { this.props.match.params.operation === "add" ?
-                        (
-                          <input
-                            onChange={ this.onChange }
-                            // defaultValue={record[field]}
-                            // readOnly={field === 'vid'}
-                            type="text"
-                            name={ field }
-                            id={ field }
-                            className="form-control"
-                          />
-                        )
-                        :
-                        (
-                          <input
-                            onChange={ this.onChange }
-                            defaultValue={ record[ field ] }
-                            readOnly={ field === 'vid' || field === 'VIN' }
-                            type="text"
-                            name={ field }
-                            id={ field }
-                            className="form-control"
-                          />
-                        )
-                      }
-                    </MDBRow>
+                    { index !== Object.keys( record ).length - 1 ?
+                      (
+                        <>
+                          <MDBRow>
+                            <label htmlFor={ field } className="grey-text">
+                              { columnHeadings[ index ].label }
+                            </label>
+                          </MDBRow>
+
+                          <MDBRow>
+                            { this.props.match.params.operation === "add" ?
+                              (
+                                <input
+                                  onChange={ this.onChange }
+                                  // defaultValue={record[field]}
+                                  // readOnly={ field === 'vid' || field === 'VIN' }
+                                  type="text"
+                                  name={ field }
+                                  id={ field }
+                                  className="form-control"
+                                />
+                              )
+                              :
+                              (
+                                <input
+                                  onChange={ this.onChange }
+                                  defaultValue={ record[ field ] }
+                                  readOnly={ field === 'vid' || field === 'VIN' }
+                                  type="text"
+                                  name={ field }
+                                  id={ field }
+                                  className="form-control"
+                                />
+                              )
+                            }
+                          </MDBRow>
+                        </>
+                      )
+                      :
+                      (
+                        <>
+                          {
+                            record[field].map( sField =>
+                              <div className="border m-5">
+                                {
+                                  Object.keys( sField ).map( ssFieldKey =>
+                                    <>
+                                      <p>Label: { ssFieldKey }</p>
+                                      <p>Value: { sField[ ssFieldKey ] }</p>
+                                    </>
+                                  )
+                                }
+                              </div>
+                            )
+                          }
+                          </>
+                      )
+                    }
                   </>
                 ) )
               )
@@ -145,13 +217,13 @@ export class EditAdd extends Component {
               )
             }
           </MDBContainer>
-          <MDBBtn
+          < MDBBtn
             color="success"
             type="submit">
             Submit
           </MDBBtn>
           <MDBBtn
-            onClick={ () => this.props.history.push( `/${this.props.dataArrayName}/` ) }
+            onClick={ () => this.props.history.push( `/${ this.props.dataArrayName }/` ) }
             color="danger">
             Cancel
           </MDBBtn>
@@ -162,3 +234,57 @@ export class EditAdd extends Component {
 }
 
 export default EditAdd;
+
+
+{/*<MDBRow>*/
+}
+{/*  { field.map( ( subfield, index ) =>*/
+}
+{/*    <>*/
+}
+{/*      <label htmlFor={ field } className="grey-text">*/
+}
+{/*        { columnHeadings[ offset + index ].label }*/
+}
+{/*      </label>*/
+}
+{/*      <p>{ subfield }</p>*/
+}
+{/*      */
+}
+{/*    </>*/
+}
+{/*  ) }*/
+}
+{/*</MDBRow>*/
+}
+
+
+{/*<MDBRow>*/
+}
+{/*  {*/
+}
+{/*    typeof field === Array*/
+}
+{/*    &&*/
+}
+{/*    field.map(subfield =>*/
+}
+{/*      <p>{subfield}</p>*/
+}
+{/*    )*/
+}
+{/*    // record.length === index*/
+}
+{/*    // &&*/
+}
+{/*    //   field.map(subfield =>*/
+}
+{/*    //   <p>{subfield}</p>*/
+}
+{/*    //   )*/
+}
+{/*  }*/
+}
+{/*</MDBRow>*/
+}
